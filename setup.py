8ec989b531c14a0cbcb1f689aaa7ded55f3c577e
JinKanai <jin@kanaisan.com>
2020-04-10 22:41:59 +0900

    fix

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 project_name = 'postkun'
 setup(name=project_name,
       version='0.0.1',
-      description='post suru hito',
       author='jinkanai',
       author_email='jin@kanaisan.com',
+      description='post suru hito',
       long_description='long description',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
+      url="https://github.com/jinkanai/postkun",
       entry_points="""
 """)
