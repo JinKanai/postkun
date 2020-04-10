4b85fc1f97b489f8b699b9008ace296486056588
JinKanai <jin@kanaisan.com>
2020-04-10 22:34:00 +0900

    pip test

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+project_name = 'postkun'
+setup(name=project_name,
+      version='0.0.1',
+      description='post suru hito',
+      author='jinkanai',
+      author_email='jin@kanaisan.com',
+      long_description='long description',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points="""
+""")
